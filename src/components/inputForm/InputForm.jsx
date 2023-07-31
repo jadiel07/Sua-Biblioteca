fcb25320ca3fb897b8f79715118ac3d2de816a90
jadiel07 <jadiel174@hotmail.com>
2023-07-31 20:07:38 -0300

    adicionamento da funcionalidade de criar cards

--- src/components/inputForm/InputForm.jsx
+++ src/components/inputForm/InputForm.jsx
@@ -1,14 +1,61 @@
+import { useState } from "react";
 import style from "./style.module.css";
 
 export default function InputForm() {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+  const [cards, setCards] = useState([]);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    if (name === "name") {
+      setName(value);
+    } else if (name === "image") {
+      setImage(value);
+    }
+  };
+
+  const handleSubbmit = (event) => {
+    event.preventDefault();
+    if (name && image) {
+      const newCard = {
+        name: name,
+        image: image,
+      };
+      setCards((prevCards) => [...prevCards, newCard]);
+      setName("");
+      setImage("");
+    }
+  };
+
   return (
     <>
       <form>
-        <input type="text" placeholder="digite o nome" />
-        <input type="text" placeholder="cole a url aqui" />
+        <input
+          type="text"
+          placeholder="digite o nome"
+          name="name"
+          value={name}
+          onChange={handleInputChange}
+        />
+        <input
+          type="text"
+          placeholder="cole a url aqui"
+          name="image"
+          value={image}
+          onChange={handleInputChange}
+        />
         <br />
-        <button>Enviar</button>
+        <button onClick={handleSubbmit}>Enviar</button>
       </form>
+      <div className={style.card}>
+        {cards.map((card, index) => (
+          <div key={index}>
+            <h1>{card.name}</h1>
+            <img src={card.image} alt={card.name} />
+          </div>
+        ))}
+      </div>
     </>
   );
 }
