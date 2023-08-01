c38ec999db331f18fe07d2f8e1d98c7416e3c3f5
jadiel07 <jadiel174@hotmail.com>
2023-08-01 11:20:32 -0300

    add novas funcionabillidades

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,12 @@
 import style from "./style.module.css";
-
+import logo from "/Logo.jpg";
 export default function Header() {
   return (
     <>
       <nav>
         <ul>
           <li>Contato</li>
-          <li>Logo</li>
+          <img src={logo} alt={logo} />
           <li>Como usar?</li>
         </ul>
       </nav>
--- src/components/inputForm/InputForm.jsx
+++ src/components/inputForm/InputForm.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import style from "./style.module.css";
 
 export default function InputForm() {
@@ -19,14 +20,29 @@ export default function InputForm() {
     event.preventDefault();
     if (name && image) {
       const newCard = {
+        id: uuidv4(),
         name: name,
         image: image,
       };
       setCards((prevCards) => [...prevCards, newCard]);
       setName("");
       setImage("");
+
+      localStorage.setItem("cards", JSON.stringify([...cards, newCard]));
     }
   };
+
+  const handleDelete = (cardId) => {
+    setCards((prevCards) => prevCards.filter((card) => card.id !== cardId));
+  };
+
+  //para carregar os dados quando usuario entrar novamente
+  useEffect(() => {
+    const storedCards = localStorage.getItem("cards");
+    if (storedCards) {
+      setCards(JSON.parse(storedCards));
+    }
+  }, []);
 
   return (
     <>
@@ -53,6 +69,7 @@ export default function InputForm() {
           <div key={index}>
             <h1>{card.name}</h1>
             <img src={card.image} alt={card.name} />
+            <button onClick={() => handleDelete(card.id)}>X</button>
           </div>
         ))}
       </div>
